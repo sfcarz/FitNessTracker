7a3850ba55b630fc7e8c2886c9025d22ba7dd50f
sfcarz <chrisarojasz2003@gmail.com>
2020-12-19 19:59:44 -0800

    first commit / finished project

---
+++ server.js
@@ -0,0 +1,31 @@
+require('dotenv/config')
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(e => console.log('root index line 13', e));
+
+
+// routes
+app.use(require("./routes/api.js"));
+app.use(require("./routes/view.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
